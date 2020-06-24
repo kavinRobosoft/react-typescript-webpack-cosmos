3e16ea30eb561ae699743c08614d0aa8a9fc242d
kavinRobosoft <kavin.s@robosoftin.com>
2020-06-25 01:50:56 +0530

    Splitted the styles into different files

--- src/__fixtures__/app.tsx
+++ src/__fixtures__/app.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import Header from './components/header/header'
 import Body from './components/body/body'
+
 import './styles/main.scss';
+import './styles/countinuewatchrow.scss';
+import './styles/header.scss';
+import './styles/herobanner.scss';
+import './styles/mystuffrow.scss';
 
 
 const  App  = () => {
